c428944aa3dd97f156dcbf629140d2ecfe355391
Abid ali <khanali3229>
2024-04-06 16:32:18 +0500

    Calculator design complete with html-css-js

---
+++ script.js
@@ -0,0 +1,18 @@
+let string = "";
+let buttons = document.querySelectorAll("#button");
+
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerText === "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (e.target.innerText === "C") {
+      string = "";
+      document.querySelector("input").value = string;
+    } else {
+      console.log(e.target);
+      string = string + e.target.innerText;
+      document.querySelector("input").value = string;
+    }
+  });
+});
